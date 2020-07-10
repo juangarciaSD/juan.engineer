a80b75f4d45a28ccfe5d72a39990325243f6696d
Juan Garcia <jgarc210@gmail.com>
2020-07-10 16:44:03 -0500

    reverted to old version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import firebase from 'firebase'
-import core from '../public/lib/core'
 import Tippy from 'tippy.js'
 import '../public/lib/firebase'
 import '../public/components/ui'
-
-globalThis.core = core
 
 function MeApp({ Component, pageProps }) {
   globalThis.firebase = firebase
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,50 +14,10 @@ import Github from '../public/assets/icons/github.svg'
 import Linkedin from '../public/assets/icons/linkedin.svg'
 import Notify from '../public/assets/icons/notify.svg'
 import Email from '../public/assets/icons/email.svg'
-import { Spotify } from '../public/lib/spotify'
-import core, { ACCESS_TOKEN } from '../public/lib/core'
 
 const SocialLink = dynamic(import('../public/components/SocialLink'))
 
 const Index = () => {
-
-  let url = "";
-  let token = ""
-  if(typeof window === 'undefined') {
-    //@ts-ignore
-    global.window = {}
-  }
-  if(process.browser) {
-    url = window.location.search
-  }
-  const urlParams = new URLSearchParams(url)
-  const code = urlParams.get('code')
-  const getToken = async() => {
-    const response = await core.SpotifyToken({ code })
-    core.state.ACCESS_TOKEN.set(response.data?.access_token)
-    //@ts-expect-error
-    core.api.config.options.headers.authorization = `Bearer ${ACCESS_TOKEN.value}`
-    console.log(`State value ${core.state.ACCESS_TOKEN.value}`)
-    console.log(response.data?.refresh_token)
-    core.state.ACCESS_TOKEN.watch('Token', () => {
-      console.log("Token has changed, now performing get data function")
-      getData()
-    })
-  }
-
-  const getData = async() => {
-    const response = await core.SpotifyData()
-    console.log(response)
-  }
-
-  console.log(`Code -> ${code}`)
-  if(code != null) {
-    const startSpotify = () => {
-      getToken()
-    }
-    startSpotify()
-  }
-
   return(
     <>
     <Head>
@@ -98,7 +58,6 @@ const Index = () => {
           <SocialLink href="mailto:me@juan.engineer" tooltip="Email">
             <Email />
           </SocialLink>
-          <button onClick={Spotify.SpotifyAuth}>Spotify login</button>
       </Social>
     </Container>
     </>
