2525fabe7534058a65602dd5627fef0d7c8556d9
Juan Garcia <jgarc210@gmail.com>
2020-07-10 16:29:26 -0500

    x

--- public/lib/spotify.ts
+++ public/lib/spotify.ts
@@ -6,15 +6,15 @@ const spotify = new SpotifyWebApi();
 
 const client_id = process.env.spotifyClientId;
 const client_secret = process.env.spotifyClientSecret;
-const redirect_uir = process.env.redirectUri;
+const redirect_uri = process.env.redirectUri;
 
-x
+const scopes = "user-read-playback-state user-read-current-playing";
 
 export const Spotify = {
   SpotifyAuth: () => {
     window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scopes=${encodeURIComponent(
       scopes
-    )}&redirect_uri=${encodeURIComponent(redirect_uir)}`;
+    )}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
   },
   SpotifyToken: () => {
       
